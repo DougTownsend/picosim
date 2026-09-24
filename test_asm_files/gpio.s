f21a451aaa77229debc86fbe2359966923232b04
Doug Townsend <townsend.doug@gmail.com>
2026-09-24 16:00:19 -0400

    working GPIO20 code

--- test_asm_files/gpio.s
+++ test_asm_files/gpio.s
@@ -10,17 +10,37 @@
  * works over USB serial on the Pico.
  */
 
+.equ gpio_basei, 0x40014000
+.equ gpio20_offset, 0xa4
+.equ gpio20_out_mask, 0x100
+
 asm_main:
     push    {r7, lr}
     sub     sp, #8
     add     r7, sp, #0
+
+    //Step 1 is getting the GPIO ctrl register address into a register (R3)
     ldr     r1, =gpio_basei
     ldr     r2, =gpio20_offset
-    add     r3, r1, r2
+    adds     r3, r1, r2'
+
+    //Step 2 is enabling the output of the GPIO by setting bits 12 and 13 to 1.
     movs    r4, #3
     lsls    r4, r4, #12
     LDR     R5, [R3, #0]
-    OR      R5, R5, R4
+    ORrs      R5, R5, R4
+
+    //Step 3 is setting bit 9 to 1. That allows us to then just control the output
+    // with bit 8. Bit 8 = 0, output 0. Bit 8 = 1, output 1.
+    movs    r4, #1
+    lsls    r4, r4, #9
+    ORrs      R5, R5, R4
+    STR     R5, [R3, #0]
+    
+    //Step 4 is setting bit 8 to 1 to turn on the LED
+    LDR     R5, [R3, #0]
+    ldr     r4, =gpio20_out_mask
+    ORrs      R5, R5, R4
     STR     R5, [R3, #0]
 
 .Lloop:
@@ -29,13 +49,6 @@ asm_main:
     beq     .Ldone
     bl      putchar         /* echo the character back           */
     b       .Lloop
-
-gpio_basei:
-    .word 0x40010004  
-gpio20_offset:
-    .word 0xa4
-
-
 
 .Ldone:
     ldr r0, =newline
